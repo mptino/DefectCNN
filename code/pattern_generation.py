8ad6feb06ab2a0a87e5ab1aef0c3183d6adad7af
mptino <mptino@github.com>
2023-03-10 17:15:15 -0500

    refactored the genFourierPattern function to develop m=2, 4, 6 patterns with varying scale and rotation!

--- code/pattern_generation.py
+++ code/pattern_generation.py
@@ -9,50 +9,20 @@ from scipy.ndimage import rotate
 import matplotlib.pyplot as plt
 import numpy as np
 
-def rotateOriginalSize(s, angle):
+def genFourierPattern(sym, lamb, theta=0):
     """
-    Rotates an discrete grid with the return value the original size.
+    Generates prototypical patterns using two-dimensional Fourier series [ref]
+    [ref] = G~H Gunaratne et al., "Pattern formation in the presence of symmetries", 1994
     
     Parameters
     ----------
-    s : np.ndarray
-        The image
-    angle : float
-        The angle to rotate by, in degrees.
-    """
-    
-    s_rot = rotate(s, angle)
-    # If the rotation changes the image shape, revert back to the original shape
-    if s_rot.shape != s.shape:
-        # Determine the padding on each side of the image caused by the rotation
-        pad = s_rot.shape[0] - s.shape[0]
-        # If 1 is padding then only one pixel is added and just remove last pixel
-        if pad == 1:
-            s_rot = s_rot[:-1, :-1]
-        # If even padding, cast to int and trim
-        elif pad % 2 == 0:
-            pad = int(pad / 2)
-            s_rot = s_rot[pad:-pad, pad:-pad]
-        # If odd padding, remove last pixel, then amend pad, cast to int, and trim
-        else:
-            s_rot = s_rot[:-1, :-1]
-            pad -= 1
-            pad = int(pad / 2)
-            s_rot = s_rot[pad:-pad, pad:-pad]
-    
-    return s_rot
-
-def genFourierPattern(a, l):
-    """
-    Generates prototypical patterns using two-dimensional Fourier
-    series.
-    
-    Parameters
-    ----------
-    a : list
-       List of floats, [a1, a2, a3]
-    l : float
+    sym : int
+       Dominant m-fold symmetry of pattern. I.e., stripe pattern m=2. Hexagonal m=6, square m=4...
+    lamb : float
         The wavelength of the pattern (unitless).
+    theta : int or float
+        The angle (in degrees) of which we wish to CCW rotate the pattern.
+        Default = 0 degrees.
     
     Returns
     -------
@@ -63,17 +33,35 @@ def genFourierPattern(a, l):
     # Define basis vectors
     e1 = np.array([1, 0])
     e2 = np.array([0, 1])
-    k1 = (2 * np.pi / l) * e2
-    k2 = (2 * np.pi / l) * ((np.sqrt(3) / 2) * e1 - 0.5 * e2)
-    k3 = (2 * np.pi / l) * (-(np.sqrt(3) / 2) * e1 - 0.5 * e2)
-    a1, a2, a3 = a[0], a[1], a[2]
+
+    # if we need to rotate basis vectors, apply rotation matrix in 2D euclidean space
+    if theta != 0:
+        theta *= np.pi / 180 # convert to radian
+        rotmat = np.array([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
+        e1 = np.dot(rotmat,e1)
+        e2 = np.dot(rotmat,e2) 
+
+    # now we need to handle the k vectors
+    if sym == 2 or sym == 6:
+        k1 = (2 * np.pi / lamb) * e2
+        k2 = (2 * np.pi / lamb) * ((np.sqrt(3) / 2) * e1 - 0.5 * e2)
+        k3 = (2 * np.pi / lamb) * (-(np.sqrt(3) / 2) * e1 - 0.5 * e2)
+        if sym == 2:
+            a1, a2, a3 = 1, 0, 0
+        elif sym == 6:
+            a1, a2, a3 = 1, 1, 1
+    elif sym == 4:
+        k1 = (2 * np.pi / lamb) * e1
+        k2 = (2 * np.pi / lamb) * e2
+        k3 = np.array([0,0]) # dumpy vector, not needed for m=4 symmetry
+        a1, a2, a3 = 1, 1, 0
+
     # Dot function for 2d as np.dot is not compatible with meshes.
     dot2d = lambda n,m: n[0] * m[0] + n[1] * m[1]
     # Define the function and return. Note np.dot is not compatible with meshes.
     f = lambda x, y: a1 * np.exp(1j * dot2d(k1, np.array([x, y]))) \
                    + a2 * np.exp(1j * dot2d(k2, np.array([x, y]))) \
                    + a3 * np.exp(1j * dot2d(k3, np.array([x, y])))
-                   
     return(f)
 
 def genMesh(F, x, y, Nx, Ny):
@@ -113,7 +101,7 @@ def genMesh(F, x, y, Nx, Ny):
     return F(X,Y)
 
 if __name__ == "__main__":
-    pattern = genFourierPattern([1,50,0], 20)
+    pattern = genFourierPattern(sym=2, lamb=30,theta=0)
     N = 250
     discPattern = np.real(genMesh(pattern, x = [-(N-1)/2.0, (N-1)/2.0], y = [-(N-1)/2.0, (N-1)/2.0], Nx = N, Ny = N))
     plt.imshow(discPattern,cmap='gray')
